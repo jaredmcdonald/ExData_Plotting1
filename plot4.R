5d675b41dec9300b30e20219aec846f85cc2d4ce
Jared McDonald <jared.r.mcdonald@gmail.com>
2015-09-13 19:26:08 -0400

    fix legend on plot4

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ build_plot4 <- function () {
     lines(DATA$dateTime, DATA$Sub_metering_2, col = 'red')
     lines(DATA$dateTime, DATA$Sub_metering_3, col = 'blue')
     legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
-           lty = c(1,1,1), col = c('black', 'red', 'blue'))
+           lty = c(1,1,1), col = c('black', 'red', 'blue'), bty = 'n', cex = 0.75)
     
     plot(DATA$dateTime, DATA$Global_reactive_power, xlab = 'datetime',
          ylab = 'Global_reactive_power', type = 'l')
